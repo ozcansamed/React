bda6de92b9f64246d360f3d5a30bca550e5aaf54
ozcansamed <ozcansamed61@gmail.com>
2020-01-29 21:23:34 +0100

    changes after review

--- week1/react-exercises/src/components/Guarantee.js
+++ week1/react-exercises/src/components/Guarantee.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-export default function Guarantee(props) {
-  console.log(props);
+export default function Guarantee({ title, description, img }) {
   return (
     <div className='service'>
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={img} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </div>
   );
 }
